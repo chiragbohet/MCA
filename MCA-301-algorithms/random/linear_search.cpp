e3f92bf5e26852a3d72c77f71fa3fb02f14428ea
Vidul <vidulkumar@gmail.com>
2019-10-31 21:33:16 +0530

    Update linear_search.cpp
    updated to accept input from user (key_to_search  ) and dynamic array size using vector

--- MCA-301-algorithms/random/linear_search.cpp
+++ MCA-301-algorithms/random/linear_search.cpp
@@ -3,38 +3,51 @@ Implementing linear search
 */
 
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 using namespace std;
 
-const int MAXSIZE = 30; //Maximum size of Array / Count of Randomly generated numbers.
+//const int MAXSIZE = 30; //Maximum size of Array / Count of Randomly generated numbers.
 const int RANGE = 100; // Range from 1 to ?
-int search = 7;	//The number to find.
+//int search = 7;	//The number to find.
 
 int main(){
 	
 	//declaring a array A
-	int A[MAXSIZE];
+	//int A[MAXSIZE];
+	int search = 0 , MAXSIZE;
+	bool found = false;
+	cout<<"Enter Array size : ";
+	cin>>MAXSIZE;
+    cout<<"Enter Key : ";
+    cin>>search;
+	
+	vector<int> A(MAXSIZE);
 	int comparisons = 0; //comparisons required to search the given number
 	
 	//Inserting random numbers
-	for(int i = 1;i<=MAXSIZE;i++){ A[i] = rand() % RANGE; }
+	for(int i = 0;i<MAXSIZE;i++){ A[i] = rand() % RANGE; }
 	
 	//printing 
-	for(int i = 1;i<=MAXSIZE;i++){ cout<<A[i]<<endl; }
+	for(int i = 0;i<MAXSIZE;i++){ cout<<A[i]<<" "; }
 	
 	
 	//Searching 
-	for(int i = 1;i<=MAXSIZE;i++){
+	for(int i = 0;i<MAXSIZE;i++){
 		
 		comparisons++;
 		
 		if(A[i] == search){
-		cout<<"Found at " << i<<"th position."<<endl;
+		cout<<"Found at " << i+1<<"th position."<<endl;
+		found = true;	
 		break;	
 		}
 		
 		
 	}
-	cout<<"Total Comparisons = "<<comparisons;
+	if(!found)
+		cout<<"\nCould Not find key \n ";
+	
+    cout<<"Total Comparisons = "<<comparisons;
 	return 0;
 }
